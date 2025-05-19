63efa19ee8d3f3840aa2025b6f8792cb30fb0f1c
Jisang <julienyu@dgist.ac.kr>
2025-05-19 19:46:40 +0900

    NEW

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from Environment import ENV
 from agent import Actor, Critic, MADDPGTrainer, ReplayBuffer
 
 # Hyperparameters
-episodes = 100000
+episodes = 1000
 timesteps = 3000
 batch_size = 256
 gamma = 0.95
