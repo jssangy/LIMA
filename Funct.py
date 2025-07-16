5832c3ff005e5f429d6bf016f6f08c683390d428
Jisang <julienyu@dgist.ac.kr>
2025-07-16 18:30:40 +0900

    NEW

--- Funct.py
+++ Funct.py
@@ -1,28 +1,13 @@
+import colorsys
+
 class Color_dict():
-    
     def __init__(self, agv_num):
-        base_colors = [
-            (255, 0, 0),       # A 빨강
-            (0, 255, 0),       # B 초록
-            (0, 0, 255),       # C 파랑
-            (255, 255, 0),     # D 노랑
-            (255, 0, 255),     # E 핑크
-            (0, 255, 255),     # F 하늘
-            (255, 165, 0),     # G 주황
-            (128, 0, 128),     # H 보라
-            (0, 128, 128),     # I 청록
-            (128, 128, 0),     # J 올리브
-            (255, 105, 180),   # K 핫핑크
-            (0, 100, 0),       # L 진녹
-            (139, 69, 19),     # M 갈색
-            (70, 130, 180),    # N 강철파랑
-            (210, 105, 30),    # O 초콜릿
-        ]
-
         self.dic = {}
-
-        for i in range(min(agv_num, 15)):  # 최대 15(A-O)개 할당 가능
-            self.dic[chr(i + 65)] = base_colors[i]
+        for i in range(agv_num):
+            h = i / agv_num
+            r, g, b = colorsys.hsv_to_rgb(h, 1, 1)
+            rgb_255 = (int(r * 255), int(g * 255), int(b * 255))
+            self.dic[chr(i + 65)] = rgb_255
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from Environment import ENV
 from GUI import *
+import argparse
 
 def main():
-    env = Environment.ENV(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--problem', '--p',
+        type=str,
+        required=True,
+        help='Problem json file path (e.g. example_problems/random.domain/random_32_32_20_100.json)'
+    )
+    args = parser.parse_args()
+
+    problem_json_path = args.problem
+
+    env = ENV(problem_json_path)
     gui = GUI(env)
     return
 
-main()
+if __name__ == "__main__":
+    main()
 
