f7fac815c5697f465f5870ab0fa6dc3c6187fdd3
JS <julienyu@dgist.ac.kr>
2025-08-25 10:45:33 +0900

    new

--- module/trainer.py
+++ module/trainer.py
@@ -208,6 +208,7 @@ class Trainer:
             wandb.init(project="smdp_mappo", config=cfg, name=run_base)
         # run.id 확보 후 최종 이름 확정(.pt 포함)
         if wandb.run:
+            full_name = f"{run_base}.pt"
             wandb.run.name = run_base    # ← W&B 대시보드 run 이름
         else:
             full_name = f"{run_base}.pt"  # W&B 미사용 시
