62051b59c31e73b72e21f2772a6a7f43a481ada1
JS <julienyu@dgist.ac.kr>
2025-09-20 21:44:19 +0900

    tjunction added

--- prob_generator/script/tasks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {0..25};
-do
-python3.10 task_generator.py --map ~/Codes/research/traffic-mapf/benchmark-lifelong/maps/sortation_small.map --n_tasks 50000 --output ~/Codes/research/traffic-mapf/benchmark-lifelong/tasks/sortation_small_$i.task
-done
