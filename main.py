619ece7b5b7f91461d30edc5bc2eafb23047d57a
JS <julienyu@dgist.ac.kr>
2025-09-22 21:25:06 +0900

    add traffic and train

--- main.py
+++ main.py
@@ -42,12 +42,12 @@ def main():
     parser.add_argument("--prob", type=str, default="cross_9")
     parser.add_argument("--algo", type=int, default=0, choices=[0,1,2,3,4],
                         help="0=BFS, 1=A*, 2=D*, 3=PIBT, 4=CBS")
-    parser.add_argument("--num-amrs", type=int, default=20)
+    parser.add_argument("--num-amrs", type=int, default=40)
     parser.add_argument("--max-arm-h", type=int, default=5)
     parser.add_argument("--max-arm-v", type=int, default=5)
     parser.add_argument("--max-steps", type=int, default=10000)
     parser.add_argument("--traffic-mode", type=str, default="task", choices=["traffic","task"])
-    parser.add_argument("--model-path", type=str, default="checkpoint/final_policy.pt")
+    parser.add_argument("--model-path", type=str, default="checkpoint/final_mlp_policy.pt")
     args = parser.parse_args()
 
     prob_path = f"problems/cross/{args.prob}.json"
